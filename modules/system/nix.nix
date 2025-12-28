7649d538ce987b3239ec38833c78372f1f00cc8a
Aleksandr <sav231189@gmail.com>
2025-12-28 13:35:13 +0500

    fix: correct hyprland cachix public key, fix boot.nix syntax

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -38,7 +38,7 @@
       
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "hyprland.cachix.org-1:a7pgxzMz7+ voices/q3gHpDQvPFtM7rJuOIhs7ljGYk+c="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
